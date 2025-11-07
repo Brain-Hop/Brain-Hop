1ba7a13fcee49776e5355f45fbfc53308efc87ea
Sarvagya Saxena <sarvagyasaxena2006@gmail.com>
2025-11-07 19:57:56 +0530

    Commit done

--- main.py
+++ main.py
@@ -187,4 +187,5 @@ def close_chat():
 
 # -------------------- RUN --------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides this PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
